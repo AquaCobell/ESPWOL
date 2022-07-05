e834f2e72223ed1888ed6ebf5aa6d1ffe0ef8686
nicrieser <nico.rieser@gmail.commkdir hello-worldcd hello-worldgit initgit statusgit config --global user.email nico.rieser@gmail.com>
2022-07-05 14:30:53 +0200

    Inital Commit

---
+++ arduino/main/main.ino
@@ -0,0 +1,176 @@
+
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <WiFiUdp.h>
+#include <WakeOnLan.h>
+#include <FS.h>
+#include <LittleFS.h>
+#include <ESP8266WiFi.h>
+#include <cstddef>
+#include <ArduinoJson.h>
+
+
+const char* ssid = "POCO F3"; //Replace with your own SSID
+const char* password = "1234567890"; //Replace with your 
+AsyncWebServer server(80);
+WiFiUDP UDP;
+WakeOnLan WOL(UDP);
+
+class Device
+{
+  private:
+  char* name;
+  char* mac;
+
+  public:
+  Device(char* name, char* mac)
+  {
+    this->name = name;
+    this->mac= mac;
+  }
+
+  Device()
+  {
+    
+  }
+
+
+
+  void setname(char* name)
+  {
+    this->name = name;
+  }
+
+  void setmac(char* mac)
+  {
+    this->mac = mac;
+  }
+
+  char* getname()
+  {
+    return this->name;
+  }
+  char* getmac()
+  {
+    return this->mac;
+  }
+  
+};
+
+
+Device devicelist[15]; //* -> Pointer, weil Objekt nicht erstellt werden kann da keine Parameter übergeben werden, Array nicht initialisiert ist. Sonst wird Array mit bereits vorhandenen Objekten erstellt.
+
+
+String ip;
+String mac;
+
+
+void setup() {
+  
+  Serial.begin(115200);
+  if(!LittleFS.begin())
+  {
+    Serial.println("LittleFS Fehler");
+    return;
+  }
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+  Serial.println(WiFi.localIP()); 
+
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+  {
+    request->send(LittleFS, "/main.html", String(), false);
+  });
+
+  server.on("/wakeup", HTTP_GET, [](AsyncWebServerRequest *request)
+  {
+    /*if(request->hasParam("ip"))
+    {
+        ip = request->getParam("ip")->value();
+    }*/
+    if(request->hasParam("mac"))
+    {
+      
+      
+        mac = request->getParam("mac")->value();
+        print(ip,mac);
+        wakeupbyString(mac);
+    }
+    request->send(LittleFS, "/main.html", String(), false);
+  });
+
+
+
+    server.begin();
+}
+
+void print(String ip, String mac)
+   {
+      Serial.println("IP:" + ip + "| MAC: "+ mac);
+   }
+
+void wakeup(char* MACAdress)
+{
+  WOL.sendMagicPacket(MACAdress);
+}
+
+void wakeupbyString(String MACAdress)
+{
+    
+         
+  char *macchar;
+  MACAdress.toCharArray(macchar, MACAdress.length() + 1);
+  Serial.println("Ich lebe noch!");
+  wakeup(macchar);
+}
+
+void wakeupbydevice(Device device)
+{
+  WOL.sendMagicPacket(device.getmac());
+}
+
+void fillArray(char* name, char* mac)
+{
+  Device dev(name,mac);
+  for(int i= 0; i<15; i++)
+  {
+    if(devicelist[i].getmac() == nullptr )
+    {
+      Serial.println("HUHUHU");
+      devicelist[i] = dev; 
+      return;
+    }
+
+  }
+}
+
+void ArrayToJson(Device* devicelist[])
+{
+  
+}
+
+void loop() 
+{
+   //Serial.print("test");
+    //Device dev("test","test");
+
+    //Serial.println(dev.getmac());
+    //char *mac2 = "E4-B9-7A-DE-00-94";
+    //wakeup(mac2);
+    //Serial.print("Woken Up");
+    //delay(3000);
+
+    //fillArray("PC","A:5C:23489038495734985");
+    //fillArray("PC2","A:5C:23254234532452345734985");
+    //fillArray("PC3","A:5C:234823452345234585");
+    //Serial.println("Devices");
+    //Serial.println(devicelist[0].getmac());
+    //Serial.println(devicelist[1].getmac());
+    //Serial.println(devicelist[2].getmac());
+}
